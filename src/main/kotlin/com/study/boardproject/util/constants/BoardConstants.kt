c0ce5e5e6fc3af27325fdd9c71ef73054049c948
sudhdkso <waga1009@gmail.com>
2024-08-01 15:55:34 +0900

    feat: 게시글 상수 추가

---
+++ src/main/kotlin/com/study/boardproject/util/constants/BoardConstants.kt
@@ -0,0 +1,8 @@
+package com.study.boardproject.util.constants
+
+object BoardConstants{
+    const val MAX_TITLE_LENGTH = 200
+    const val MAX_CONTENT_LENGTH = 1000
+
+    const val MAX_EDITABLE_DAYS = 10
+}
