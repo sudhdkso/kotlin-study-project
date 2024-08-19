f5f0d818485aea6470c823269859805e740bfcee
sudhdkso <waga1009@gmail.com>
2024-08-19 16:59:37 +0900

    fix : 엔티티명 변경에 따른 테스트 코드 수정 누락에 따른 오류 해결

--- src/test/kotlin/com/study/boardproject/board/BoardFixtures.kt
+++ src/test/kotlin/com/study/boardproject/board/BoardFixtures.kt
@@ -1,7 +1,7 @@
 package com.study.boardproject.board
 
-import com.study.boardproject.board.dto.BoardRequestDto
-import com.study.boardproject.board.entity.Board
+import com.study.boardproject.board.dto.PostRequestDto
+import com.study.boardproject.board.entity.Post
 import com.study.boardproject.board.entity.User
 
 private val TITLE: String = "제목"
@@ -9,7 +9,7 @@ private val CONTENT: String = "내용"
 private val WRITER: User = createUser()
 
 fun createBoard(title:String = TITLE, content:String = CONTENT, writer:User = WRITER)
-: Board = Board(title,content, writer)
+: Post = Post(title,content, writer)
 
 fun createBoardRequest(title:String = TITLE, content:String = CONTENT, email:String= EMAIL)
-: BoardRequestDto = BoardRequestDto(title, content, email)
+: PostRequestDto = PostRequestDto(title, content, email)
--- src/test/kotlin/com/study/boardproject/board/CommentFixtures.kt
+++ src/test/kotlin/com/study/boardproject/board/CommentFixtures.kt
@@ -1,18 +1,18 @@
 package com.study.boardproject.board
 
 import com.study.boardproject.board.dto.CommentRequestDto
-import com.study.boardproject.board.entity.Board
 import com.study.boardproject.board.entity.Comment
+import com.study.boardproject.board.entity.Post
 import com.study.boardproject.board.entity.User
 
 private val CONTENT = "댓글 내용"
 private val DEPTH = 0
-private val BOARD_ID = 1L;
+private val POST_ID = 1L;
 
 private val WRITER: User = createUser()
-private val BOARD: Board = createBoard()
-fun createCommentRequest(content:String = CONTENT, depth:Int = DEPTH, boardId:Long = BOARD_ID) : CommentRequestDto
- = CommentRequestDto(content, depth, boardId, EMAIL)
+private val POST: Post = createBoard()
+fun createCommentRequest(content:String = CONTENT, depth:Int = DEPTH, postId:Long = POST_ID) : CommentRequestDto
+ = CommentRequestDto(content, depth, postId, EMAIL)
 
-fun createComment(content:String = CONTENT, depth:Int = DEPTH, board:Board = BOARD, writer:User = WRITER) : Comment
-= Comment(content, depth, board, writer)
+fun createComment(content:String = CONTENT, depth:Int = DEPTH, post: Post = POST, writer:User = WRITER) : Comment
+= Comment(content, depth, post, writer)
