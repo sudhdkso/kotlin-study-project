493a1aaff33fae79981fcf76e4ab403e8580a0d8
sudhdkso <waga1009@gmail.com>
2024-09-29 20:53:56 +0900

    feat: 매니저 레벨만 게시판 생성 및 수정할 수 있도록 기능 추가

---
+++ src/main/kotlin/com/study/boardproject/core/annotation/CheckAccessLevel.kt
@@ -0,0 +1,28 @@
+package com.study.boardproject.core.annotation
+
+import com.study.boardproject.board.user.entity.enums.Level
+import com.study.boardproject.board.user.service.UserService
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CheckAccessLevel(val requiredLevels: IntArray)
+
+@Aspect
+@Component
+class AccessLevelAspect(private val userService: UserService) {
+    @Around("@annotation(checkAccessLevel)")
+    fun checkAccessLevel(joinPoint: ProceedingJoinPoint, checkAccessLevel: CheckAccessLevel): Any? {
+        val user = userService.getCurrentUser()
+        val userLevel = user?.level ?: Level.EMPTY
+
+        if (!checkAccessLevel.requiredLevels.any { it <= userLevel.value }) {
+            throw IllegalAccessException("접근 권한이 없습니다.")
+        }
+
+        return joinPoint.proceed()
+    }
+}
